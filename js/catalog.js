2956be537e288ceda86dc2c377c0faaad2bb94b3
Crypt <72517634+Angel-crypt@users.noreply.github.com>
2025-06-08 21:10:42 -0600

    (feat): design joke catalog cards with animations and styles

--- js/catalog.js
+++ js/catalog.js
@@ -3,9 +3,15 @@ if (sessionStorage.getItem('isLoggedIn') !== 'true') {
     window.location.href = '../index.html';
 }
 
+function revealDelivery(element) {
+  element.classList.add('revealed');
+  element.classList.remove('blurred');
+}
+
+
 const cardContainer = document.getElementById('joke-cards');
 
-fetch("https://v2.jokeapi.dev/joke/Any?lang=en&amount=10")
+fetch("https://v2.jokeapi.dev/joke/Any?lang=en&amount=100")
     .then(res => res.json())
     .then(data => {
         const amount = data.amount;
@@ -17,16 +23,30 @@ fetch("https://v2.jokeapi.dev/joke/Any?lang=en&amount=10")
 
             let content = '';
 
-            switch (joke.type) {
-                case 'twopart':
-                    content = `<p>${joke.setup}</p><p>${joke.delivery}</p>`;
-                    break;
-                case 'single':
-                    content = `<p>${joke.joke}</p>`;
-                    break;
-                default:
-                    content = `<p>Tipo desconocido de chiste.</p>`;
-            }
+            if (joke.type === 'twopart') {
+                content = `
+                  <div class='book'>
+                    <div class="page">
+                        <div class="side left">
+                        <p class="setup">${joke.setup}</p>
+                        </div>
+                        <div class="side right blurred" onclick="revealDelivery(this)">
+                        <p class="delivery">${joke.delivery}</p>
+                        </div>
+                    </div>
+                  </div>
+                `;
+              } else if (joke.type === 'single') {
+                content = `
+                  <div class="single">
+                    <div class="page one-side">
+                      <p class="joke">${joke.joke}</p>
+                    </div>
+                  </div>
+                `;
+              } else {
+                content = `<p>Tipo desconocido de chiste.</p>`;
+              }
 
             card.innerHTML = content;
             cardContainer.appendChild(card);
